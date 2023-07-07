4d5c04a1cb1102356d08997ca40b2de431fbf8ef
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2023-07-07 11:18:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import json
+import numpy as np
+import torch
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.tokenizer = AutoTokenizer.from_pretrained('tiiuae/falcon-7b')
+        self.generator = pipeline(
+            'text-generation',
+            model='tiiuae/falcon-7b',
+            tokenizer=self.tokenizer,
+            torch_dtype=torch.bfloat16,
+            trust_remote_code=True,
+            device=0,
+        )
+
+    def infer(self, inputs):
+        pipeline_output = self.generator(
+            inputs['prompt'],
+            max_length=200,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=self.tokenizer.eos_token_id,
+        )
+        generated_text = pipeline_output[0]['generated_text']
+        return {"generated_text": generated_text}
+
+    def finalize(self):
+        self.pipe = None
